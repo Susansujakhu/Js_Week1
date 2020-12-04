f1aa9299efebe6e8710a41ab096aa283c5ed486f
susansujakhu <sushansujakhu14@gmail.com>
2020-12-04 21:44:09 +0545

    Finish Assignment

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ function myFunction() {
 	var hours= date.getHours();
 	if (hours == 0){ hours = 12};
 	var minutes = date.getMinutes();
-	if (minutes == 0){ minuter = 12};
 	var seconds = date.getSeconds();
-	if (seconds == 0){ seconds = 12};
 	document.getElementById("time").innerHTML = hours+ ':' + minutes + ':' + seconds;
 	document.getElementById("date").innerHTML = date.getDate() + ' ' + month + ' ' + date.getFullYear() + ' ' + day;
 
@@ -30,4 +28,59 @@ function calcCircumfrence() {
 		console.log(2*3.14*radius);
 		document.getElementById('Result').innerText = "The Circumference is : " + result;
 	}
+}
+
+window.onload = function(){
+	array_elements();
+}
+
+
+
+function array_elements() {
+	var fav_place = ["Biratnagar", "Lalitpur", "Patan", "Kathmandu","Pokhara","Palpa"];
+
+	fav_place.forEach( function(value, index){
+		document.getElementById('default_array').innerHTML += "<li>"+value+"</li>"; 
+		//console.log(value);
+	});
+	var point;
+	fav_place.splice(4, 0, "Bhaktapur", "Banepa");
+	fav_place.forEach( function(value, index){
+		document.getElementById('add_2_places').innerHTML += "<li>"+value+"</li>";
+		if (value == 'Kathmandu'){
+			point = index;
+		}
+		//console.log(value);
+	});
+	//console.log(fav_place);
+		
+	fav_place.splice(point,1);
+	fav_place.forEach( function(value, index){
+		document.getElementById('remove_ktm').innerHTML += "<li>"+value+"</li>"; 
+		//console.log(value);
+	});
+	//console.log(fav_place);
+
+	document.getElementById('array_length').innerHTML = "The Lenth of Array is : " + fav_place.length;
+
+	}
+
+
+
+function tips_calculate() {
+	var amount = document.getElementById('amount').value;
+	var percent = document.getElementById('percent').value;
+	if (amount == '') {
+		document.getElementById('amount').style.border = '2px solid red';
+		document.getElementById('tips').innerText = '';
+	}
+	else if (percent == '') {
+		document.getElementById('percent').style.border = '2px solid red';
+		document.getElementById('tips').innerText = '';
+	}
+	else {
+	var tips_amount = (amount * percent) / 100;
+	console.log(tips_amount);
+	document.getElementById('tips').innerText = "Tips Amount is Rs. " + tips_amount;
+}
 }
